6de4c2e3f2a0d827c41e49de0e18308216edfe0f
philippvujic <newtimestube@gmail.com>
2019-01-28 18:26:23 +0100

    MailReceiver

--- app/Http/Controllers/MailReceiverController.php
+++ app/Http/Controllers/MailReceiverController.php
@@ -8,13 +8,21 @@
 
 namespace App\Http\Controllers;
 
+use App\MailReceiver;
 use Illuminate\Http\Request;
 
 
 class MailReceiverController extends Controller
 {
     public function create(Request $request) {
-        csrf_token();
-        return $request->all();
+        $mail = new MailReceiver;
+        $mail->Content_Type = request('Content-Type');
+        $mail->Date = request('Date');
+        $mail->From = request('From');
+        $mail->In_Reply_To = request('In_Reply_To');
+        $mail->Subject = request('Subject');
+        $mail->to = request('to');
+        $mail->body_html = request('body_html');
+        $mail->save();
     }
 }
---
+++ database/migrations/2019_01_28_164705_mails.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Mails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Content_Type')->nullable();
+            $table->date('Date')->nullable();
+            $table->string('From')->nullable();
+            $table->string('In_Reply_To')->nullable();
+            $table->string('Subject')->nullable();
+            $table->string('to')->nullable();
+            $table->string('body_html')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
