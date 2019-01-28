6593ae85f4476abeb9da599d51b37d23a39f5f8c
phil <newtimestube@gmail.com>
2019-01-28 22:05:49 +0100

    Modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,3 +14,4 @@ require('./bootstrap');
  */
 
 require('./components/Main');
+require('./components/MyModal');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,17 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @csrf
     <title>Pig Travel</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
 
 <body>
 <div id="app"></div>
+
+<div id="modal"></div>
+
 <script src="/js/app.js"></script>
 </body>
 </html>
