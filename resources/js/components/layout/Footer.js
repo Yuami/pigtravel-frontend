610c815d735d77c9cc60470470f627205675a881
JoanFornesRayo <j_fornes_1999@hotmail.com>
2019-02-03 16:34:54 +0100

    Footer added and Router

--- resources/js/components/layout/Footer.js
+++ resources/js/components/layout/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
         const support = [
             {
                 value: "support.contact",
-                link: ""
+                link: "support"
             }
         ];
 
