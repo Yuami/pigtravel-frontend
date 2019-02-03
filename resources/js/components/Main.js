31644136539a3f91951c13388b0e42a6f5903ece
JoanFornesRayo <j_fornes_1999@hotmail.com>
2019-02-03 16:22:20 +0100

    Footer done

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import { LocaleContext } from "../LocaleContext.js";
 import Header from "./layout/Header";
+import {BrowserRouter, Switch, Route} from "react-router-dom"
 import axios from 'axios';
 import Footer from "./layout/Footer";
+import Home from "../Views/Home";
+import AboutUs from "../Views/AboutUs";
 import * as ReactDOM from "react-dom";
 
 
@@ -22,6 +25,10 @@ export default class Main extends Component {
         return (
             <LocaleContext.Provider value={this.state.preferredLocale}>
                 <Header changeLanguage={this.changeLanguage} />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about-us" component={AboutUs} />
+                </Switch>
                 <Footer/>
             </LocaleContext.Provider>
         );
@@ -29,5 +36,9 @@ export default class Main extends Component {
 }
 
 if (document.getElementById('app')) {
-    ReactDOM.render(<Main/>, document.getElementById('app'));
+    ReactDOM.render(
+        <BrowserRouter>
+        <Main/>
+        </BrowserRouter>,
+        document.getElementById('app'));
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,24 +3,23 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Pig Travel</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 </head>
 <body>
 
-<div id="app">
+<div id="app"></div>
 
-</div>
-
+<script defer src="https://use.fontawesome.com/releases/v5.7.1/js/all.js" integrity="sha384-eVEQC9zshBn0rFj4+TU78eNA19HMNigMviK/PU/FFjLXqa/GKPgX58rvt5Z8PLs7" crossorigin="anonymous"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-<script src="/js/app.js">
+<script src="{{ asset('js/app.js') }}"></script>
 
-</script>
 </body>
 </html>
