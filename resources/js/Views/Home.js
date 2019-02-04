f281d9d697ae1c0b095e16725188bea83144efd0
JoanFornesRayo <j_fornes_1999@hotmail.com>
2019-02-04 22:03:55 +0100

    Footer Down

--- resources/js/Views/Home.js
+++ resources/js/Views/Home.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import Translate from "../lang/Translate";
+import Searcher from "../components/Searcher";
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <h1><Translate type="general" string="home"/></h1>
+                <Searcher/>
             </div>
         );
     }
