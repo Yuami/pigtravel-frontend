ad96b4811db8c7b46fce8e346d3c517aaa477fb5
PepToniBibiloni <peptonibibiloni@gmail.com>
2019-02-05 13:53:35 +0100

    carrousel,resev

--- resources/js/Views/Home.js
+++ resources/js/Views/Home.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
 import Translate from "../lang/Translate";
+import TitleInicio from "../components/specific/TitleInicio";
+import Searcher from "../components/layout/Searcher";
+import CarouselInicio from "../components/layout/CarouselInicio";
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <h1><Translate type="general" string="home"/></h1>
+                <div className="jumbotron_cont">
+                    <TitleInicio/>
+                    <Searcher/>
+                </div>
+                <div id="recomendacionCiudades">
+                    <div id="textoRecomendacion">
+                        <h2><Translate type={"carrousel"} string={"recomendacion"}/></h2>
+                    </div>
+                </div>
+                <CarouselInicio/>
             </div>
         );
     }
--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -11,6 +11,7 @@ import TitleInicio from "./specific/TitleInicio";
 import Searcher from "./layout/Searcher";
 import MainModal from "./layout/MainModal";
 import Terms from "./layout/Terms";
+import BookingDetail from "../Views/BookingDetail";
 
 
 export default class Main extends Component {
@@ -39,12 +40,9 @@ export default class Main extends Component {
         return (
             <LocaleContext.Provider value={this.state.locale}>
                 <Header changeLanguage={this.changeLanguage} />
-                <div className="jumbotron_cont">
-                <TitleInicio/>
-                <Searcher/>
-                </div>
                 <Switch>
                     <Route exact path="/" component={Home}/>
+                    <Route exact path="/bookings" component={BookingDetail}/>
                     <Route exact path="/about-us" component={AboutUs}/>
                     <Route exact path="/modal">
                         <MainModal buttonLabel="Reservate" />
@@ -60,7 +58,7 @@ export default class Main extends Component {
 if (document.getElementById('app')) {
     ReactDOM.render(
         <BrowserRouter>
-        <Main/>
+            <Main/>
         </BrowserRouter>,
         document.getElementById('app'));
 }
