f5f833280a8d90e26d36cc6ee1ac49cbd8144949
philippvujic <newtimestube@gmail.com>
2019-02-07 15:38:56 +0100

    Merge branches 'master' and 'phil' of https://github.com/IESMANACOR/front-lloguer-vacacional
    # Conflicts:
    #	public/js/app.js
    #	resources/js/components/layout/Header.js
    #	routes/api.php

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-<<<<<<< HEAD
+
+
 Route::get('/reservas', 'ReservasController@show');
-=======
 Auth::routes();
 Route::get('/reservas', 'ReservasController@index');
->>>>>>> a95885beaa5b2a62eb35d8b326a44f3a09265b51
 Route::get('/reservas/{reserva}', 'ReservasController@show');
 
 Route::get('/verify', 'TokenController@index');
