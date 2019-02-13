51e81ec528b274dc720f45d971d65bb879172cb1
JoanFornesRayo <j_fornes_1999@hotmail.com>
2019-02-13 13:28:52 +0100

    Laravel Vivienda relations models

--- app/Cities.php
+++ app/Cities.php
@@ -9,14 +9,25 @@ class Cities extends Model
 {
     protected $table = 'cities';
 
-    static function getIfHaveHouses(){
+    public function region()
+    {
+        return $this->belongsTo(Region::class);
+    }
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    static function getIfHaveHouses()
+    {
         $cities = DB::table('cities')
-            ->select('cities.id','cities.name','regions.name as province')
+            ->select('cities.id', 'cities.name', 'regions.name as province')
             ->distinct()
             ->join('vivienda', 'cities.id', '=', 'vivienda.idCiudad')
-            ->join('regions','regions.id','=','cities.region_id')
+            ->join('regions', 'regions.id', '=', 'cities.region_id')
             ->get();
-      return $cities;
+        return $cities;
     }
 }
 
