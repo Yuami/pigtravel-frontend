2a7c60d035053531a78956415474c51912346148
JoanFornesRayo <j_fornes_1999@hotmail.com>
2019-02-18 09:46:59 +0100

    refactor Searcher

--- resources/js/components/layout/Searcher.js
+++ resources/js/components/layout/Searcher.js
@@ -1,13 +1,9 @@
 import React, {Component} from 'react';
 import Translate from "../../lang/Translate";
-import {Col, Label, Button, Popover, PopoverBody, InputGroup} from 'reactstrap';
+import {Col, Label, Button, Popover, PopoverBody} from 'reactstrap';
 import DatePickerInicio from "../specific/DatePickerInicio"
 import FaIcon from "../general/FaIcon";
-import {LocaleContext} from "../../LocaleContext";
-import {translate} from "../../helpers";
 import AutocompleteCity from "../specific/AutocompleteCity";
-import FormButton from "../general/Forms/FormButton";
-import Form from "reactstrap/es/Form";
 import FormGroup from "reactstrap/es/FormGroup";
 import originalMoment from "moment";
 import {extendMoment} from "moment-range";
@@ -61,45 +57,66 @@ class Searcher extends Component {
             <Button color="" className="incrementIcon" onClick={this.DecreaseItem}><FaIcon
                 icon={'fa fa-minus'}/></Button>;
 
+        const place = (
+                <FormGroup>
+                    <Label>
+                        <FaIcon icon={'fa fa-globe'}/>
+                    </Label>
+                    <div id="location">
+                        <AutocompleteCity change={this.handleChangePlace.bind(this)} place={this.state.place}/>
+                    </div>
+                </FormGroup>
+        );
+
+        const guests = (
+            <FormGroup id={"guests"}>
+                <Label><FaIcon icon={'fa fa-user'}/></Label>
+                <div className="inputSearcher">
+                    {this.state.clicks} <input type="hidden" name="guests" value={this.state.clicks}/>
+                    <Translate string={this.state.clicks === 1 ? 'guest' : 'guests'} type={'searcher'}/>
+                </div>
+                <Popover placement="bottom" isOpen={this.state.show} target="guests"
+                         toggle={this.ToggleDiv} trigger="legacy">
+                    <PopoverBody>
+                        {decreaseBtn}
+                        <Button color="" className="incrementIcon" onClick={this.IncrementItem}>
+                            <FaIcon icon={'fa fa-plus'}/>
+                        </Button>
+                    </PopoverBody>
+                </Popover>
+            </FormGroup>
+        );
+
+        const linkButtonData = {
+            pathname: "/search",
+            state: {
+                guests: this.state.clicks,
+                place: this.state.place,
+                date: this.state.date
+            }
+        };
+
+        const submit = (
+            <Link to={linkButtonData}>
+                <Button color="primary" className="SearcherIcon">
+                    <FaIcon icon={'fa fa-search'} size={'fa-2x'}/>
+                </Button>
+            </Link>
+        );
+
         return (
             <Col md="10" lg="8" sm="8" xs="10" className="buscador shadow">
                 <Col md="6" lg="5" sm="12" xs="12" className="filtro">
-                    <FormGroup>
-                        <Label>
-                            <FaIcon icon={'fa fa-globe'}/>
-                        </Label>
-                        <div id="location">
-                            <AutocompleteCity change={this.handleChangePlace.bind(this)} place={this.state.place}/>
-                        </div>
-                    </FormGroup>
+                    {place}
                 </Col>
                 <Col md="6" lg="4" sm="12" xs="12" className="filtro">
                     <DatePickerInicio onChange={this.handleChangeDates.bind(this)} value={this.state.date}/>
                 </Col>
                 <Col md="6" lg="2" sm="12" xs="12" className="filtro">
-                    <FormGroup id={"guests"}>
-                        <Label><FaIcon icon={'fa fa-user'}/></Label>
-                        <div className="inputSearcher">
-                            {this.state.clicks} <input type="hidden" name="guests" value={this.state.clicks}/>
-                            <Translate string={this.state.clicks === 1 ? 'guest' : 'guests'} type={'searcher'}/>
-                        </div>
-                        <Popover placement="bottom" isOpen={this.state.show} target="guests"
-                                 toggle={this.ToggleDiv} trigger="legacy">
-                            <PopoverBody>
-                                {decreaseBtn}
-                                <Button color="" className="incrementIcon" onClick={this.IncrementItem}>
-                                    <FaIcon icon={'fa fa-plus'}/>
-                                </Button>
-                            </PopoverBody>
-                        </Popover>
-                    </FormGroup>
+                    {guests}
                 </Col>
                 <Col md="6" lg="1" sm="12" xs="12" className="form-group">
-                    <Link to={"/search"}>
-                        <Button color="primary" className="SearcherIcon">
-                            <FaIcon icon={'fa fa-search'} size={'fa-2x'}/>
-                        </Button>
-                    </Link>
+                    {submit}
                 </Col>
             </Col>
         );
