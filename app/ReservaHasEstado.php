d46cc97f079c8dd09ce54f95ecffcef7dc76036f
phil <newtimestube@gmail.com>
2019-02-22 10:05:31 +0100

    Enviar Correos al reservar, insertar estado, insertar mensaje a la BD

---
+++ app/ReservaHasEstado.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class ReservaHasEstado extends Model
+{
+    protected $table = 'reserva_has_estado';
+    const CREATED_AT = 'fechaCambio';
+    const UPDATED_AT = 'fechaCambio';
+}
+
