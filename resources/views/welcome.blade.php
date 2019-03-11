537c88029a9d4ac2255e2cef8a51804f329607c6
joanfornesrayo <j_fornes_1999@hotmail.com>
2019-03-11 20:47:43 +0100

    Google Analitycs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136095531-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-136095531-1');
+    </script>
 
 
     <meta charset="utf-8">
