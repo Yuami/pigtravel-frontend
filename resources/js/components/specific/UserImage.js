9a33e2369f791cb161e637a241ad57f4372ec66a
PepToniBibiloni <peptonibibiloni@gmail.com>
2019-03-11 13:49:10 +0100

    favicon

--- resources/js/components/specific/UserImage.js
+++ resources/js/components/specific/UserImage.js
@@ -1,8 +1,6 @@
-
 import React, {Component} from 'react';
 import PropTypes from "prop-types";
 import axios from "axios";
-
 
 
 class UserImage extends Component {
@@ -14,23 +12,34 @@ class UserImage extends Component {
             image: [],
         };
     }
+
     componentWillMount() {
         axios.get('/api/fotoPerfil/' + this.props.idUser)
-            .then(user=>
-                this.setState({
-                image: user.data
-            }));
+            .then(user => {
+                if (user.data) {
+                    this.setState({
+                        image: user.data
+                    });
+                } else {
+                    this.setState({
+                        image: [{"path": "/assets/uploads/img/perfiles/default-image.png"}]
+                    });
+                }
+            });
     }
-    render() {
 
-            return (
-                <>
-                    <img className="img-circle img-profile" src={"http://admin.pigtravel.top/assets/uploads/img/perfiles/default-image.png"}/>
-                </>
-            );
+    render() {
+        console.log(this.state.image);
+        return (
+            <>
+                <img className="img-circle img-profile"
+                     src={"http://admin.pigtravel.top/assets/uploads/img/perfiles/default-image.png"}/>
+            </>
+        );
 
     }
 }
+
 UserImage.propTypes = {
     idUser: PropTypes.number.isRequired,
 };
